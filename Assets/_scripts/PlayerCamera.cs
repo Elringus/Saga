e41536f8e062c9a6a5aa354a931646118335824a
Elringus <elringus@yandex.ru>
2014-07-10 19:10:33 +0400

    add photon

---
+++ Assets/_scripts/PlayerCamera.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+	public Transform Target;
+	public float XSpeed;
+	public float YSpeed;
+	public float ZoomSpeed;
+
+	public float XMinLimit;
+	public float XMaxLimit;
+
+	public float MinDistance;
+	public float MaxDistance;
+
+	public float DefaultXAngle;
+
+	[HideInInspector]
+	public Transform Transform;
+	private float currentDistance;
+	private float targetDistance;
+	private float y;
+	private float x;
+
+	private void Awake ()
+	{
+		Application.targetFrameRate = 60;
+
+		Transform = transform;
+		targetDistance = MaxDistance / 3;
+	}
+
+	private void Start ()
+	{
+		x = DefaultXAngle;
+		y = 180 + Target.eulerAngles.y;
+	}
+
+	private void LateUpdate ()
+	{
+		if (Target)
+		{
+			if (Input.GetMouseButton(1))
+			{
+				Screen.lockCursor = true;
+
+				y += Input.GetAxis("Mouse X") * XSpeed;
+				x -= Input.GetAxis("Mouse Y") * YSpeed;
+			}
+			else
+			{
+				Screen.lockCursor = false;
+			}
+
+			targetDistance = Mathf.Clamp(targetDistance - Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed, MinDistance, MaxDistance);
+			currentDistance = Mathf.Lerp(currentDistance, targetDistance, Time.deltaTime * 5);
+
+			x = ClampAngle(x, XMinLimit, XMaxLimit);
+
+			RaycastHit hit;
+			if (Physics.SphereCast(Target.transform.position, .15f, (Transform.position - (Target.transform.position)).normalized, out hit, currentDistance, 1 << 8)) currentDistance = hit.distance;
+
+			Transform.position = new Vector3(
+				Target.position.x + currentDistance * Mathf.Sin(y * Mathf.Deg2Rad) * Mathf.Cos(x * Mathf.Deg2Rad),
+				Target.position.y + currentDistance * Mathf.Sin(x * Mathf.Deg2Rad),
+				Target.position.z + currentDistance * Mathf.Cos(y * Mathf.Deg2Rad) * Mathf.Cos(x * Mathf.Deg2Rad)
+			);
+
+			Transform.LookAt(Target.position);
+		}
+	}
+
+	private float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F) angle += 360F;
+		if (angle > 360F) angle -= 360F;
+		return Mathf.Clamp(angle, min, max);
+	}
+}
---
+++ Assets/_scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public Transform Transform;
+	public float MoveSpeed;
+	public float PointMoveLimit;
+
+	private float currentMoveSpeed;
+	private Vector3 prevPosition;
+	private Vector3 currentMovePoint;
+	private CharacterController controller;
+	private Animator animator;
+
+	private void Awake () 
+	{
+		Transform = transform;
+		controller = GetComponent<CharacterController>();
+		animator = GetComponentInChildren<Animator>();
+		currentMovePoint = Transform.position;
+	}
+
+	private void Update () 
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, (1 << 8) | (1 << 9)))
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				if (hit.collider.gameObject.layer == 8) currentMovePoint = hit.point;
+				//if (hit.collider.gameObject.layer == 9 && hit.collider.gameObject.GetComponent<Player>()) Target = hit.collider.gameObject.GetComponent<Actor>();
+			}
+
+			if (Input.GetMouseButtonDown(1))
+			{
+				//if (hit.collider.gameObject.layer == 9) ((IInteractiveObject)hit.collider.GetComponent(typeof(IInteractiveObject))).Activate(this);
+			}
+		}
+
+		if (Vector3.Distance(Transform.position, currentMovePoint) > PointMoveLimit)
+		{
+			Transform.LookAt(currentMovePoint);
+			Transform.eulerAngles = new Vector3(0, Transform.eulerAngles.y, 0);
+			controller.Move(Transform.TransformDirection(Vector3.forward) * Time.deltaTime * MoveSpeed);
+		}
+
+		if (!controller.isGrounded) controller.Move(Vector3.down * Time.deltaTime * 9f);
+
+		currentMoveSpeed = (Transform.position - prevPosition).sqrMagnitude;
+		prevPosition = Transform.position;
+
+		Animate();
+	}
+
+	private void Animate ()
+	{
+		animator.SetFloat("Forward", currentMoveSpeed * MoveSpeed * 50, .1f, Time.deltaTime);
+		animator.SetBool("InAir", !controller.isGrounded);
+		//Animator.SetBool("Climbing", IsClimbing);
+		//Animator.SetBool("Fighting", InBattle);
+	}
+}
